f9030766cb96e4eb4dac6410b90e1008d2c18aca
Joel Sontag <jsontag4@student.touro.edu>
2019-04-08 12:47:49 -0400

    added a search for availible CPU IP

--- src/RAID/Master_Server.java
+++ src/RAID/Master_Server.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.*;
 
-public class Master_Server
-{
+public class Master_Server {
 
 	private static Scanner kb = new Scanner(System.in);
 	private static volatile HashSet<File> files = new HashSet<>();
@@ -17,9 +18,9 @@ public class Master_Server
 	private static volatile Master_Server ms;
 	private static volatile Master master;
 	private static volatile Server server;
+	private static ArrayList<String> availibleCPUs = new ArrayList<String>();
 
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException {
 		print("RAID Server initializing...\n\n");
 		ms = new Master_Server();
 		master = ms.new Master();
@@ -30,37 +31,31 @@ public class Master_Server
 		kb.close();
 	}
 
-	private static void print(Object o)
-	{
+	private static void print(Object o) {
 		System.out.println(o);
 	}
 
-	private static void menu()
-	{
+	private static void menu() {
 		int choice;
-		do
-		{
+		do {
 			print("Main menu\n0. Shutdown RAID Server\n1. View stats");
 			choice = choiceValidator(0, 2);
-			switch (choice)
-			{
-				case 1:
-					print(files.size() + " files are being stored");
-					print("1 client is connected");
-					print(master.getSlaveCount() + " slaves are connected");
-					break;
-				default:
-					break;
+			switch (choice) {
+			case 1:
+				print(files.size() + " files are being stored");
+				print("1 client is connected");
+				print(master.getSlaveCount() + " slaves are connected");
+				break;
+			default:
+				break;
 			}
 		} while (choice != 0);
 		exit = true;
 	}
 
-	private static int choiceValidator(int low, int high)
-	{
+	private static int choiceValidator(int low, int high) {
 		int choice;
-		do
-		{
+		do {
 			choice = kb.nextInt();
 			if (choice < low || choice > high)
 				System.out.println("\nInvalid choice!\n" + low + " - " + high);
@@ -69,30 +64,25 @@ public class Master_Server
 		return choice;
 	}
 
-	private class Master extends Thread
-	{
+	private class Master extends Thread {
 		private HashSet<String> slaves = new HashSet<>();
 
-		public int getSlaveCount()
-		{
+		public int getSlaveCount() {
 			return slaves.size();
 		}
 
 	}
 
-	private class Server extends Thread
-	{
+	private class Server extends Thread {
 		private Socket socket;
 		private ServerSocket listener;
 		private PrintWriter out;
 		private BufferedReader in;
 
-		public void run()
-		{
+		public void run() {
 			files.add(new File("test1", "10/23/1910", "Moshe"));
 			files.add(new File("test2", "09/17/2001", "Moshe"));
-			try
-			{
+			try {
 
 				listener = new ServerSocket(436);
 				socket = listener.accept();
@@ -100,21 +90,16 @@ public class Master_Server
 				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				System.out.println("Server connected to client @ " + socket.getInetAddress().getHostAddress());
 				listen();
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 
-		private void listen() throws IOException
-		{
-			while (!exit)
-			{
+		private void listen() throws IOException {
+			while (!exit) {
 				ArrayList<String> data = new ArrayList<>();
 				int choice = Integer.parseInt(in.readLine());
-				do
-				{
+				do {
 					data.add(in.readLine());
 				} while (!data.get(data.size() - 1).equals("$$$"));
 				data.remove(data.size() - 1);
@@ -122,24 +107,50 @@ public class Master_Server
 			}
 		}
 
-		private void send(int choice, ArrayList<String> data)
-		{
-			switch (choice)
-			{
-				case 1:	// View list of files
-					files.stream().forEachOrdered(out::println);
-					break;
-				case 2:	// Add file
-					break;
-				case 3:	// Get file
-					break;
-				case 4:	// Remove file
-					break;
-				default:
-					out.println("ERROR!");
-					break;
+		private void send(int choice, ArrayList<String> data) {
+			switch (choice) {
+			case 1: // View list of files
+				files.stream().forEachOrdered(out::println);
+				break;
+			case 2: // Add file
+				break;
+			case 3: // Get file
+				break;
+			case 4: // Remove file
+				break;
+			default:
+				out.println("ERROR!");
+				break;
 			}
 			out.println("$$$");
+		}
+	}
+
+	public static void getAvailibleSlaves() {
+		InetAddress localhost;
+		try {
+			localhost = InetAddress.getLocalHost();
+			// this code assumes IPv4 is used
+			byte[] ip = localhost.getAddress();
+
+			for (int i = 1; i <= 254; i++) {
+				ip[3] = (byte) i;
+				InetAddress address;
+				try {
+					address = InetAddress.getByAddress(ip);
+
+					if (address.isReachable(1000)) {
+						availibleCPUs.add(address.toString());
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 

e0b3d892cbdacac63f861c1edec16ee4ee5c2d76
mo523 <43011781+mo523@users.noreply.github.com>
2019-05-13 19:28:06 -0400

    Created bat file for easy deployment

---
+++ Compile & Run.cmd
@@ -0,0 +1,22 @@
+@echo off
+set /P compile="(D)ownload, compile & run or just (R)un? "
+set /P program="(R)AID Server, (C)lient, (S)lave? "
+if "%compile%" == "D" (
+rmdir raid /s /q
+git clone https://github.com/mo523/RAID.git
+cd raid
+git checkout Same_Ports
+cd src
+javac RAID/*.java
+)
+cls
+if "%program%" == "R" (
+java RAID/RAID_Server
+) else (
+   if "%program%" == "C" (
+	java RAID/Client
+   ) else (
+	java RAID/Slave
+   )
+)
+@pause
